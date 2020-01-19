366b4f9dfd3bdbdfb6fc13490da93dfe07374af6
Frank Moses <frank@apexpotentials.com>
2020-01-19 10:22:07 +0100

    added api to views.py and called it on home.html

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,19 @@
 # this is my view.py file 
 from django.shortcuts import render
+
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests 
+	
+	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20129&distance=25&API_KEY=66681BC7-E6FD-4B3F-99BF-1753ABEA4121")
+	
+	try:
+		api = json.loads(api.request.content)
+
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api': api})	
 
 def about(request):
 	return render(request, 'about.html', {})
