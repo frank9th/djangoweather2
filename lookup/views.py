2246e9a120828ceea8d93d40b8ec01dabac7843b
Frank Moses <frank@apexpotentials.com>
2020-01-21 14:46:07 +0100

    api change at views.py git push clear

--- lookup/views.py
+++ lookup/views.py
@@ -5,13 +5,13 @@ def home(request):
 	import json
 	import requests 
 	
-	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20129&distance=25&API_KEY=66681BC7-E6FD-4B3F-99BF-1753ABEA4121")
+	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=66681BC7-E6FD-4B3F-99BF-1753ABEA4121")
 	
 	try:
-		api = json.loads(api.request.content)
+		api = json.loads(api_request.content)
 
 	except Exception as e:
-		api = "Error..."
+		api = "Error... unable to connet at the moment"
 
 	return render(request, 'home.html', {'api': api})	
 
